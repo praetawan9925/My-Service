ec5f2e78856daa8cd3a395787303d846fc1c14ee
student08 <praetawan9925@snru.ac.th>
2017-06-15 16:30:16 +0700

    Initial Commit

---
+++ My Service/AddNewUserViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AddNewUserViewController.swift
+//  My Service
+//
+//  Created by Student08 on 6/15/2560 BE.
+//  Copyright © 2560 SNRU. All rights reserved.
+//
+
+import UIKit
+
+class AddNewUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("This is AddNewUser")
+        
+        
+        
+        
+    } // Main Method
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    } // didReceive
+    
+
+    
+
+} //Main Class
