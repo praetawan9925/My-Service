f0bfe89859a35aa4010f7fffd00522dc4f38aef1
student08 <praetawan9925@snru.ac.th>
2017-06-16 16:26:20 +0700

    16 June 2560

--- My Service/ViewController.swift
+++ My Service/ViewController.swift
@@ -11,46 +11,50 @@ import UIKit
 class ViewController: UIViewController {
     // Explicit
     var myString = ""
+    var strUser: String? = nil
+    var strPassword: String? = nil
+    var strMyJSON = ""
     
     
+    
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBOutlet weak var alertLabel: UILabel!
+    
+    
+    @IBAction func loginButton(_ sender: Any) {
+        //Get Value From TextField
+        strUser = userTextField.text
+        strPassword = passwordTextField.text
+        
+        //Check Space
+        if strUser?.characters.count == 0 || strPassword?.characters.count == 0 {
+            alertLabel.alpha = 1
+        
+        } else {
+            
+          alertLabel.alpha = 0
+            let strUrl = "http://androidthai.in.th/snru/getUserWhereprae.php?isAdd=true&User="+strUser!+""
+            print("strUrl ==> \(strUrl)")
+            readJSON(strURL: strUrl)
+            
+            
+            
+        } // if
+        
+        
+    } //loginButton
     
     
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Process Connected Http
-        let urlPHP = "http://swiftcodingthai.com"
-        
-        //Chang String to url String
-        let myURL = URL(string: urlPHP)
-        
-        let request = NSMutableURLRequest(url: myURL!)
-        let task = URLSession.shared.dataTask(with: request as URLRequest) {
-            data, response, error in
-            
-            if error != nil {
-                print("Error ==> \(error)")
-            }   else {
-                
-                if let unwrappedData = data {
-                 
-                    let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
-                    let strJSON = dataString as Any
-                    print("strJSON ==> \(strJSON)")
-                
-                
-                }
-                //
-            
-            } // if1
-            
-            
-        
-        
-        
-        }
-        task.resume()
         
         
         
@@ -60,6 +64,92 @@ class ViewController: UIViewController {
         
         
     } //Main Method
+    
+    func readJSON(strURL:String) -> Void {
+        
+        // Process Connected Http
+        let urlPHP = strURL
+        
+        //Chang String to url String
+        let myURL = URL(string: urlPHP)
+        
+        let request = NSMutableURLRequest(url: myURL!)
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            data, response, error in
+            
+            if error != nil {
+                print("Error ==> \(String(describing: error))")
+            }   else {
+                
+                if let unwrappedData = data {
+                    
+                    let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                    let strJSON = dataString as Any
+                    print("strJSON ==> \(strJSON)")
+                    
+                    if dataString != nil {
+                        print("dataString not nil")
+                        
+                        } else {
+                         print("dataString nil")
+                              }
+                    
+                    
+                    }
+                    
+                    
+                    }
+            
+                    
+                    let strKey = ","
+                    if let myContentArray = dataString?.components(separatedBy: strKey) {
+                        
+                   
+                    print("myContentArray[3] ==> \(myContentArray[3])")
+                        
+                        
+                        
+                      // Check Password
+                        let myPassword = "\"Password\":\"" + self.strPassword! + "\"
+                        if strPassword == myContentArray[3] {
+                        // Password True
+                        print("Password True")
+                        performSegue(withIdentifier: "GoToMap", sender: <#T##Any?#>)
+                            
+                        } else {
+                        //Password False
+                            print("Password False")
+                        
+                        
+                        }
+                        
+                        
+                    
+                    } // if
+                    
+                    
+                    
+                    
+                    
+                }
+                
+                
+            } // if1
+            
+            
+        }
+        task.resume()
+        
+        
+        
+    } // readJSON
+    
+
+
+    
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
